89b1527df43803ff8586423a62aa0367256fa5e1
NAOKI SUGIHARA <naoki.sugihara@livline.jp>
2015-02-26 20:15:35 +0900

    import sku...stable

--- html/ebay/import_sku.php
+++ html/ebay/import_sku.php
@@ -9,13 +9,13 @@ if(isset($_GET) && $_GET["exec"]==4)
     $file = file_get_contents($path, 'r');
     $file = mb_convert_encoding($file, 'utf-8', 'sjis');
     $rows = explode("\n", $file);
+var_dump($rows);exit;
     foreach($rows as $r)
     {
         $contents[] = explode(',', $r);
     }
 
     // $contents = explode(',', $file);
-var_dump($contents);exit;
 }
 else
 {
